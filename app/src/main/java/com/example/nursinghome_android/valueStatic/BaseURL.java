d87eebe82648b32686f2a60713973794633cae1a
Quang Dương <Quangduong19992001@gmail.com>
2024-06-15 17:42:45 +0700

    .
    .

--- app/src/main/java/com/example/nursinghome_android/valueStatic/BaseURL.java
+++ app/src/main/java/com/example/nursinghome_android/valueStatic/BaseURL.java
@@ -4,7 +4,7 @@ import android.net.Uri;
 
 public class BaseURL {
     //dung cho demo gv
-//    public static String baseURL = "http://192.168.43.167:8080";
-    public static String baseURL = "http://192.168.3.102:8080";
+    public static String baseURL = "http://192.168.43.167:8080";
+//    public static String baseURL = "http://192.168.3.102:8080";
 
 }
